3ed5c3b6faaec4bbc6dc4bd36f2775215be9168b
Jonit Joseph <47573623+jonitjoseph@users.noreply.github.com>
2020-07-12 11:25:27 +0530

    express app

--- contract.sol
+++ contract.sol
@@ -9,18 +9,18 @@ contract Students{
         uint groupno;
     }
     
-    mapping(uint=>students)  rollno;
+    mapping(uint=>students)  s_id;
     
     function set (uint _rollno, string memory _name, string memory _gender, uint _age, string memory _haddress, uint _groupno) public {
-        rollno[_rollno]=students(_name,_gender,_age,_haddress,_groupno);
+        s_id[_rollno]=students(_name,_gender,_age,_haddress,_groupno);
     }
     
     function get(uint _rollno) public view returns(string memory _name, string memory _gender, uint _age, string memory _haddress, uint _groupno){
-        _name = rollno[_rollno].name;
-        _gender = rollno[_rollno].gender;
-        _age = rollno[_rollno].age;
-        _haddress = rollno[_rollno].haddress;
-        _groupno = rollno[_rollno].groupno;
+        _name = s_id[_rollno].name;
+        _gender = s_id[_rollno].gender;
+        _age = s_id[_rollno].age;
+        _haddress = s_id[_rollno].haddress;
+        _groupno = s_id[_rollno].groupno;
         //return ();
     }
 }
